d972e77491c966d848a47fe1435409ffe583d8d0
Jia-Han Wu <jiahan.wu@icloud.com>
2024-12-29 15:20:05 +0800

    Initial Commit

---
+++ PCMPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PCMPlayer
+//
+//  Created by Jia-Han Wu on 2024/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PCMPlayer/PCMPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PCMPlayerApp.swift
+//  PCMPlayer
+//
+//  Created by Jia-Han Wu on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct PCMPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
